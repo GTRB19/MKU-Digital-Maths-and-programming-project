27b22b819a1cfc9ee7d9189dbfb5df9e77423b04
benjaminharmel <benjiharmel@gmail.com>
2025-05-13 10:14:34 +0100

    Final push for database, full basic database built and basic transactions written, further will be added for logic later

--- Main file.py
+++ Main file.py
@@ -1,10 +1,12 @@
 import sqlite3
+from datetime import date
 
 conn = sqlite3.connect('MKU_digital_assignment.db')
 cursor = conn.cursor()
 
 
-def add_category(category_name, category_budget):
+# ------------------------------------------SQL Commands for category---------------------------------------------------
+def add_category(category_name, category_budget):  # Adding categories and if they exist giving the option to update
     cursor.execute(f"Select CategoryID from Category where CategoryName = '{category_name}'")
     exists = cursor.fetchone()
     if exists is None:
@@ -14,18 +16,50 @@ def add_category(category_name, category_budget):
     else:
         question = input("Already exists, do you want to update y/n?").lower()
         if question == "y":
-            cursor.execute(
-                f"Update Category set CategoryBudget = {category_budget} where CategoryName = '{category_name}'")
-            conn.commit()
+            update_category(category_name, category_budget)
         else:
             print("No Query executed")
 
 
-def remove_category(category_name):
+def remove_category(category_name, new_category):  # Removing categories, new category can be other
+    question = input("Do you want to remove relevant transactions y/n?")
+    if question == "y":
+        cursor.execute(
+            f"Delete from Spending where CategoryID = (Select CategoryID from Category where CategoryName = '{category_name}')")
+        conn.commit()
+    else:
+        change_category(category_name, new_category)
     cursor.execute(f"Delete from Category where CategoryName = '{category_name}'")
     conn.commit()
 
-# Add an update function
 
-add_category("Shopping", 100)
-remove_category("Shopping")
+def update_category(category_name, category_budget):
+    cursor.execute(
+        f"Update Category set CategoryBudget = {category_budget} where CategoryName = '{category_name}'")
+    conn.commit()
+
+
+def change_category(old_cat, new_cat):
+    cursor.execute(f"Update Category set CategoryName = '{new_cat}' where CategoryName = '{old_cat}'")
+    conn.commit()
+
+
+# -----------------------------------------SQL Commands for Spending----------------------------------------------------
+
+def add_transaction(CategoryID, Amount):
+    date_now = date.today()
+    cursor.execute(f"Insert into Spending (CategoryID, Amount, Date) VALUES ('{CategoryID}','{Amount}','{date_now}')")
+    conn.commit()
+
+
+def monthly_return(Date):
+    Date = Date[:7] + "%"
+    print(Date)
+    cursor.execute(f"Select * from Spending where Date like '{Date}'")
+    print(cursor.fetchall())
+
+
+# ---------------Any Bulk major changes, hopefully smaller per transaction can be fulfilled in GUI----------------------
+
+
+
