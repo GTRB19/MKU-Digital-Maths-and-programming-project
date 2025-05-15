a3734e457d09f50b8c3997c4048779b4b4a4a536
GTRB19 <135638407+GTRB19@users.noreply.github.com>
2025-05-15 12:57:03 +0100

    The difficult buttons are now created, it should be a simple data viewer and basic buttons. However due to the local variables used to create category buttons (this allows the same function to be executed over again to create them) they are now static and can't be changed so updating them is impossible. One way to get around may be to wipe the widgets, then rewrite. Even though not the most efficient it's only one small window meaning performance impact will still be better than 5 different if statements running every time.

--- Main file.py
+++ Main file.py
@@ -1,5 +1,8 @@
+import calendar
 import sqlite3
+import tkinter as tk
 from datetime import date
+from tkinter import ttk
 
 import pandas as pd
 
@@ -55,8 +58,10 @@ def add_transaction(CategoryID, Amount):
 
 
 def monthly_return(Date):
-    Date = Date[:7] + "%"
-    print(Date)
+    if len(Date) > 7:
+        Date = Date[:7] + "%"
+    else:
+        Date = Date + "%"
     cursor.execute(f'''Select t1.CategoryName, t1.CategoryBudget, t2.Amount
         from Category t1
         join Spending t2 on t1.CategoryID = t2.CategoryID
@@ -69,7 +74,7 @@ def monthly_return(Date):
 
 # -------------------------------------------------------Logic code------------------------------------------------------
 
-def return_monthly_dashboard(Date_selected):
+def return_monthly_dashboard(Date_selected):  # Check may be needed to ensure correct date format
     Percentage_list = []
     Spending = {
         'Category': [],
@@ -78,9 +83,7 @@ def return_monthly_dashboard(Date_selected):
     }
     df = pd.DataFrame(Spending)
     Data = monthly_return(Date_selected)
-
     for record in Data:  # Writing the database to panda
-        print(record[0])
         if (df == record[0]).any().any():  # Checks if category already in the record
             row_index = df.loc[df['Category'] == record[0]].index[0]
             cell_value = df['Spent'].loc[df.index[row_index]] + record[2]
@@ -88,7 +91,6 @@ def return_monthly_dashboard(Date_selected):
         else:
             New_row = [record[0], record[1], record[2]]
             df.loc[len(df)] = New_row
-
     for row in df.itertuples():
         percentage = round(row.Spent / row.Budget * 100)
         Percentage_list.append(percentage)
@@ -97,3 +99,73 @@ def return_monthly_dashboard(Date_selected):
     df["Percentage"] = Percentage_list
     return df
 
+
+# -----------------------------------------------------------------------------------------------------------------------
+
+
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
+
+        # configure the root window
+        self.title('Budget Tracker')
+        self.geometry('800x600')
+        self.label = tk.Label(self, text='Budget Tracker')
+        self.label.pack()
+        self.setup_date()
+        self.percentage()
+
+        # self.combo_box.bind("<<ComboboxSelected>>", lambda: self.percentage)
+        # self.combo_box_2.bind("<<ComboboxSelected>>", lambda: self.percentage)  # Might have to remove all widgets, then rewrite due to the buttons being local variables
+
+
+    def start(self):
+        self.setup_date()
+        self.percentage()
+
+    def setup_date(self):  # This will need to return an update
+        months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October",
+                  "November", "December"]
+        years = ["2020", "2021", "2022", "2023", "2024", "2025"]  # If I get time make it automatically update years
+        current_date = str(date.today())[:7].split("-")
+        self.combo_box = ttk.Combobox(self, values=months, )
+        self.combo_box.set(calendar.month_name[int(current_date[1])])
+        self.combo_box.place(x=5, y=50)
+        self.combo_box_2 = ttk.Combobox(self, values=years)
+        self.combo_box_2.set(current_date[0])
+        self.combo_box_2.place(x=185, y=50)
+
+#--------------------------------------------------------------------------------------------#
+    def percentage(self):
+        month = str(self.combo_box.current() + 1)
+        if len(month) == 1:
+            month = "0" + month
+        date = str(self.combo_box_2.get()) + "-" + month
+        data = return_monthly_dashboard(date)
+        data = data.sort_values(by = ["Percentage"], ascending=False)
+        print(data)
+        count = 0
+        xcor = 60
+        for row in data.itertuples():
+            if count == 5: #Only top 5 results shown
+                break
+            print(row.Category, row.Percentage)
+            self.create_percentage_bar(row.Category,row.Percentage, xcor,  120, count) #Autonamte numbers
+            count += 1
+            xcor += 140
+
+    def create_percentage_bar(self, category, percentage, x,y, count): # Count may be needed, as issues may arise with buttons
+        tempbar = ttk.Progressbar()
+        tempbar.place(x = x, y = y)
+        tempbar.step(percentage)
+        tempbutton = tk.Button(self, text = category, command=lambda: self.buttonclicked(tempbutton))
+        tempbutton.place(x = x, y = y+30)
+
+    def buttonclicked(self, buttonname): # This can be used to bring up certain categories
+        print(buttonname.cget('text'))
+
+#-------------------------------------------------------------------------------------------------------------------#
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
